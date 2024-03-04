e41473099e7a0d775a12449cc2c542d0cc7b02e5
Emm4nuell <eduardo.eesf@gmail.om>
2024-03-03 22:26:58 -0300

    Criado projeto SisCompras

---
+++ src/main/java/br/com/siscompras/SiscomprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.siscompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscomprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siscompras/SiscomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siscompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
