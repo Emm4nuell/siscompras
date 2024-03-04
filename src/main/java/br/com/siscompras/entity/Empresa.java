92d9548496232cab3708439454777b79d61e75b8
Emm4nuell <eduardo.eesf@gmail.om>
2024-03-03 23:23:31 -0300

    Entity EMPRESA adicionado

---
+++ src/main/java/br/com/siscompras/entity/Empresa.java
@@ -0,0 +1,32 @@
+package br.com.siscompras.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String cnpj;
+    private String nome;
+    private String fantasia;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cep;
+    private String bairro;
+    private String municipio;
+    private String uf;
+    private String email;
+    private String telefone;
+    private String celular;
+    private boolean status;
+}
