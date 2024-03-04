183005803fd379d36baaa419a3c31cb7798260ee
Emm4nuell <eduardo.eesf@gmail.om>
2024-03-04 09:06:25 -0300

    Criado a classe USUARIOCONTROLLER e USUARIODTO

---
+++ src/main/java/br/com/siscompras/controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package br.com.siscompras.controller;
+
+import br.com.siscompras.dto.UsuarioDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+}
---
+++ src/main/java/br/com/siscompras/dto/UsuarioDto.java
@@ -0,0 +1,78 @@
+package br.com.siscompras.dto;
+
+import br.com.siscompras.entity.Usuario;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDto {
+
+    private Long id;
+    @NotBlank(message = "Campo nome é obrigatório!")
+    private String nome;
+    @CPF(message = "CPF inválido!")
+    @NotBlank(message = "Campo CPF é obrigatório!")
+    private String cpf;
+    private String rg;
+    private String sexo;
+    @Email(message = "E-mail inválido!")
+    @NotBlank(message = "Campo E-mail é obrigatório!")
+    private String email;
+    private String contato;
+    private Date datanascimento;
+    private LocalDateTime datacadastro;
+    private String nivelacesso;
+    private String senha;
+    private boolean status;
+
+    public static UsuarioDto toUsuarioDto(Usuario usuario){
+        UsuarioDto dto = new UsuarioDto(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getCpf(),
+                usuario.getRg(),
+                usuario.getSexo(),
+                usuario.getEmail(),
+                usuario.getContato(),
+                usuario.getDatanascimento(),
+                usuario.getDatacadastro(),
+                usuario.getNivelacesso(),
+                usuario.getSenha(),
+                usuario.isStatus()
+        );
+
+        return dto;
+    }
+
+    public static Usuario toUsuario(UsuarioDto dto){
+        Usuario usuario = new Usuario(
+                dto.getId(),
+                dto.getNome(),
+                dto.getCpf(),
+                dto.getRg(),
+                dto.getSexo(),
+                dto.getEmail(),
+                dto.getContato(),
+                dto.getDatanascimento(),
+                dto.getDatacadastro(),
+                dto.getNivelacesso(),
+                dto.getSenha(),
+                dto.isStatus(),
+                null
+        );
+
+        return usuario;
+    }
+    
+}
