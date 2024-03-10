1e15a4bb4b30b0c35d2c4a1e97828a159de406b1
Emm4nuell <eduardo.eesf@gmail.om>
2024-03-10 12:20:37 -0300

    Feito as configurações iniciais do SpringSecurity no projeto

--- src/main/java/br/com/siscompras/config/SpringSecurityConfig.java
+++ src/main/java/br/com/siscompras/config/SpringSecurityConfig.java
@@ -1,8 +1,33 @@
 package br.com.siscompras.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(customizer -> {
+            customizer.anyRequest().permitAll();
+        })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
