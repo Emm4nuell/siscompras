14d0f0ae8143efe0c98836f91b62742ae143f735
Emm4nuell <eduardo.eesf@gmail.com>
2024-03-17 21:24:05 -0300

    Tratando o erro MethodArgumentNotValidException

--- src/main/java/br/com/siscompras/exception/HandlerException.java
+++ src/main/java/br/com/siscompras/exception/HandlerException.java
@@ -4,10 +4,15 @@ import br.com.siscompras.exception.ErrorException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class HandlerException {
@@ -23,5 +28,24 @@ public class HandlerException {
                 null);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorException> methodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest http){
+
+        List<FieldError> field = exception.getBindingResult().getFieldErrors();
+        List<ValidException> valid = field.stream()
+                .map((e) -> new ValidException(e.getField(), e.getDefaultMessage())).collect(Collectors.toList());
+
+
+        ErrorException error = new ErrorException(
+                "Validation Error",
+                "Validation Error",
+                http.getRequestURI().toString(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now(),
+                valid
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
